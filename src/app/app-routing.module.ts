b9ae83f5ab86dfd3532f461cb86fb72c8b68d572
John-Kimani <kimwaweru100@gmail.com>
2022-02-13 21:47:39 +0300

    intergrates routing in my application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'search', component: SearchComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
