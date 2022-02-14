1a1f7f59722aa4143a6a70cd7e5882cebe2f5c7a
John-Kimani <kimwaweru100@gmail.com>
2022-02-14 19:19:09 +0300

    creates new class for form component

---
+++ src/app/form.ts
@@ -0,0 +1,15 @@
+export class Form {
+    public image: any
+    public username: any
+    public bio: any
+    public followers: any
+    public following: any
+    public location: any
+    public twitter!: string
+
+    public name: any
+    public full_name:any
+    public description:any
+    public created_at: any
+    public html_url: any
+}
