7e5a730ecd1c9911960d6ac2ab73368040f48243
John-Kimani <kimwaweru100@gmail.com>
2022-02-15 00:57:05 +0300

    creates form service

--- src/app/formservice.service.ts
+++ src/app/formservice.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Form } from './form';
+import { fetchedRepos, Form } from './form';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormserviceService {
 
-  username: any
+  username!: any
   person!: Form
   link = "https://api.github.com/users/"
 
+
   constructor(private http: HttpClient) {
-    this.username = 'John-Kimani'
     this.person = new Form()
    }
 
    findNewUser(){
     let newUser = new Promise((resolve, reject) =>{
       this.http.get<any>(this.link + this.username).toPromise().then(
-        (        response: { avatar_url: any; name: any; bio: any; followers: any; following: any; location: any; twitter_username: string; full_name: any; description: any; created_at: any; html_url: any; }) =>{
+        (        response: { avatar_url: any; name: any; bio: any; followers: any; following: any; location: any; twitter_username: string}) =>{
           this.person.image = response.avatar_url
           this.person.username = response.name
           this.person.bio = response.bio
@@ -27,18 +27,42 @@ export class FormserviceService {
           this.person.following = response.following
           this.person.location = response.location
           this.person.twitter = response.twitter_username
-
-          this.person.name = response.name
-          this.person.full_name = response.full_name
-          this.person.description = response.description
-          this.person.created_at = response.created_at
-          this.person.html_url = response.html_url
-          resolve(console.log("Person and repositories can be fetched"))
+          console.log(this.person)
+          resolve(response)
         },error =>{
           reject(console.log('Information cant be fetched'))
         }
       )
     })
     return newUser
+   }
+
+              //properties for newrepos
+              newRepos:any[] = []
+              revs!: any
+              reps = new fetchedRepos()
+              url = "https://api.github.com/users/"
+
+
+   findNewRepos(){
+     let newRepos = new Promise((resolve, reject) =>{
+      this.http.get<any>(this.url + this.username + '/repos').toPromise().then(
+        response =>{
+          response.forEach((response:any) =>{
+            this.reps = new fetchedRepos()
+            this.revs.name = response.name
+            this.revs.full_name = response.full_name
+            this.revs.description = response.description
+            this.revs.created_at = response.created_at
+            this.revs.html_url = response.html_url
+            this.newRepos.push(this.reps)
+            resolve(console.log("New Repos infomation can be fetched"))
+          });
+        },error =>{
+          reject(console.log('New repositories cant be found'))
+        }
+      )
+     })
+     return newRepos
    }
 }
